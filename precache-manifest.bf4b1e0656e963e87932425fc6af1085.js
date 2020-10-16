505ba69ebea948c70ace84a12a7e6043a6605c6f
Jinyoung Ahn <48481448+zeroaan@users.noreply.github.com>
2020-10-16 12:46:46 +0900

    Updates

---
+++ precache-manifest.bf4b1e0656e963e87932425fc6af1085.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35e8626246af2976c32e106963ea1a4f",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "978363337199bc78df02",
+    "url": "/todolist/static/css/main.973d764e.chunk.css"
+  },
+  {
+    "revision": "d4307a0825624ccfe312",
+    "url": "/todolist/static/js/2.9aa2ba0b.chunk.js"
+  },
+  {
+    "revision": "33490309eac47256b827830c802fd4a5",
+    "url": "/todolist/static/js/2.9aa2ba0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "978363337199bc78df02",
+    "url": "/todolist/static/js/main.4c5112fe.chunk.js"
+  },
+  {
+    "revision": "197f51c8578d731c6831",
+    "url": "/todolist/static/js/runtime-main.e7725399.js"
+  }
+]);
