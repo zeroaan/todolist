80e4d133bf61f9a6c42e95ed7c6b79d8b61beb7f
Jinyoung Ahn <48481448+zeroaan@users.noreply.github.com>
2021-01-17 21:36:02 +0900

    Delete: redux promise, thunk

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,6 @@
-import PromiseMiddleware from "redux-promise";
-import ReduxThunk from "redux-thunk";
-import { createStore, applyMiddleware } from "redux";
-import Reducer from "./reducers";
+import { createStore } from "redux"
+import Reducer from "./reducers"
 
-const createStoreWithMiddleware = applyMiddleware(
-  PromiseMiddleware,
-  ReduxThunk
-)(createStore);
+const store = createStore(Reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
-export const store = createStoreWithMiddleware(
-  Reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+export default store
