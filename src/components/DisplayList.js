67629057f2493fa10c73b8a9f84c04a4dddaf074
Jinyoung Ahn <48481448+zeroaan@users.noreply.github.com>
2021-01-19 18:58:04 +0900

    Add: list scroll > 5

--- src/components/DisplayList.js
+++ src/components/DisplayList.js
@@ -1,17 +1,39 @@
 import React from "react"
 import { useSelector } from "react-redux"
+import styled from "styled-components"
 
 import DisplayListItem from "./DisplayListItem"
 import VisibleList from "./VisibleList"
+
+const DivList = styled.div`
+  max-width: 800px;
+  max-height: 320px;
+  overflow: auto;
+
+  &::-webkit-scrollbar {
+    width: 10px;
+    height: 0;
+  }
+  &::-webkit-scrollbar-thumb {
+    background-color: rgba(0, 175, 175, 0.5);
+    border-radius: 10px;
+  }
+  &::-webkit-scrollbar-track {
+    background-color: rgb(218, 219, 226);
+    border-radius: 10px;
+  }
+`
 
 const DisplayList = () => {
   const { todo } = useSelector((store) => store.todo)
 
   return (
     <>
-      {todo.map((item, index) => (
-        <DisplayListItem key={item.text + index} textItem={item.text} index={index} />
-      ))}
+      <DivList>
+        {todo.map((item, index) => (
+          <DisplayListItem key={item.text + index} textItem={item.text} index={index} />
+        ))}
+      </DivList>
       {todo.length > 0 && <VisibleList />}
     </>
   )
