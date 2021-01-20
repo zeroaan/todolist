c9de5d4ef0dc67118aa3685309a8a4f2617c7d21
Jinyoung Ahn <48481448+zeroaan@users.noreply.github.com>
2021-01-20 21:29:04 +0900

    Add: todo reducer immer

--- src/store/actions/todo.js
+++ src/store/actions/todo.js
@@ -1,48 +1,48 @@
-import * as types from "./types";
+import * as types from "./types"
 
 export const addList = (text) => {
   return {
     type: types.ADD_LIST,
     text: text,
-  };
-};
+  }
+}
 export const deleteList = (id) => {
   return {
     type: types.DELETE_LIST,
     id: id,
-  };
-};
+  }
+}
 export const editList = (editText, id) => {
   return {
     type: types.EDIT_LIST,
     id: id,
     editText: editText,
-  };
-};
+  }
+}
 export const completeList = (id) => {
   return {
     type: types.COMPLETE_LIST,
     id: id,
-  };
-};
+  }
+}
 
 export const visibleAll = () => {
   return {
     type: types.VISIBLE_ALL,
-  };
-};
+  }
+}
 export const visibleActive = () => {
   return {
     type: types.VISIBLE_ACTIVE,
-  };
-};
+  }
+}
 export const visibleCompleted = () => {
   return {
     type: types.VISIBLE_COMPLETED,
-  };
-};
+  }
+}
 export const clearCompleted = () => {
   return {
     type: types.CLEAR_COMPLETED,
-  };
-};
+  }
+}
--- src/store/reducers/todo.js
+++ src/store/reducers/todo.js
@@ -9,136 +9,33 @@ const todoReducer = (state = initialState, action) => {
   return produce(state, (draft) => {
     switch (action.type) {
       case types.ADD_LIST:
-        draft.todo.unshift({ text: action.text, complete: false })
+        draft.todo.unshift({ text: action.text, complete: false, invisible: false })
+        break
+      case types.DELETE_LIST:
+        draft.todo.splice(action.id, 1)
+        break
+      case types.EDIT_LIST:
+        draft.todo[action.id].text = action.editText
+        break
+      case types.COMPLETE_LIST:
+        draft.todo[action.id].complete = !state.todo[action.id].complete
+        break
+      case types.VISIBLE_ALL:
+        draft.todo.map((v) => (v.invisible = false))
+        break
+      case types.VISIBLE_ACTIVE:
+        draft.todo.map((v) => (v.complete ? (v.invisible = true) : (v.invisible = false)))
+        break
+      case types.VISIBLE_COMPLETED:
+        draft.todo.map((v) => (v.complete ? (v.invisible = false) : (v.invisible = true)))
+        break
+      case types.CLEAR_COMPLETED:
+        draft.todo = state.todo.filter((v) => !v.complete)
         break
       default:
         break
     }
   })
-  /*
-  let newState = {}
-  let newTodo = []
-  let i = 0
-  switch (action.type) {
-    case types.ADD_LIST:
-      newTodo = [{ text: action.text, complete: false }, ...state.todo]
-      newState = { ...state, todo: newTodo }
-      return newState
-
-    case types.DELETE_LIST:
-      while (i < state.todo.length) {
-        if (i !== action.id) {
-          newTodo = [...newTodo, state.todo[i]]
-        }
-        i = i + 1
-      }
-      newState = { ...state, todo: newTodo }
-      return newState
-
-    case types.EDIT_LIST:
-      while (i < state.todo.length) {
-        if (i !== action.id) {
-          newTodo = [...newTodo, state.todo[i]]
-        } else {
-          newTodo = [...newTodo, { text: action.editText, complete: false }]
-        }
-        i = i + 1
-      }
-      newState = { ...state, todo: newTodo }
-      return newState
-
-    case types.COMPLETE_LIST:
-      while (i < state.todo.length) {
-        if (i !== action.id) {
-          newTodo = [...newTodo, state.todo[i]]
-        } else {
-          newTodo = [...newTodo, { text: state.todo[i].text, complete: !state.todo[i].complete }]
-        }
-        i = i + 1
-      }
-      newState = { ...state, todo: newTodo }
-      return newState
-
-    case types.VISIBLE_ALL:
-      while (i < state.todo.length) {
-        newTodo = [
-          ...newTodo,
-          {
-            text: state.todo[i].text,
-            complete: state.todo[i].complete,
-            invisible: false,
-          },
-        ]
-        i = i + 1
-      }
-      newState = { ...state, todo: newTodo }
-      return newState
-
-    case types.VISIBLE_ACTIVE:
-      while (i < state.todo.length) {
-        if (state.todo[i].complete === false) {
-          newTodo = [
-            ...newTodo,
-            {
-              text: state.todo[i].text,
-              complete: state.todo[i].complete,
-              invisible: false,
-            },
-          ]
-        } else {
-          newTodo = [
-            ...newTodo,
-            {
-              text: state.todo[i].text,
-              complete: state.todo[i].complete,
-              invisible: true,
-            },
-          ]
-        }
-        i = i + 1
-      }
-      newState = { ...state, todo: newTodo }
-      return newState
-
-    case types.VISIBLE_COMPLETED:
-      while (i < state.todo.length) {
-        if (state.todo[i].complete === false) {
-          newTodo = [
-            ...newTodo,
-            {
-              text: state.todo[i].text,
-              complete: state.todo[i].complete,
-              invisible: true,
-            },
-          ]
-        } else {
-          newTodo = [
-            ...newTodo,
-            {
-              text: state.todo[i].text,
-              complete: state.todo[i].complete,
-              invisible: false,
-            },
-          ]
-        }
-        i = i + 1
-      }
-      newState = { ...state, todo: newTodo }
-      return newState
-
-    case types.CLEAR_COMPLETED:
-      while (i < state.todo.length) {
-        if (state.todo[i].complete === false) {
-          newTodo = [...newTodo, state.todo[i]]
-        }
-        i = i + 1
-      }
-      newState = { ...state, todo: newTodo }
-      return newState
-
-    default:
-      return state
-  }*/
 }
 
 export default todoReducer
